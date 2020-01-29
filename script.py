b037fa736a8e5ec76b687f0d0755d009146b49bd
uchida-h <60396107+uchida-h@users.noreply.github.com>
2020-01-30 01:08:11 +0900

    Create script.py

---
+++ script.py
@@ -0,0 +1,19 @@
+In [3]: num1 = input("input num1: ")
+input num1: （10）
+
+In [4]: print(num1)
+10
+
+In [5]: num2 = input("input num2: ")
+input num2: 20
+
+In [6]: print(num2)
+20
+
+In [7]: num1 = int(num1)
+
+In [8]: num2 = int(num2)
+
+In [9]: ans = num1 + num2
+
+In [10]: print(ans)
